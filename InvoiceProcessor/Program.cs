cd614ca2f8a4e7c44abd0aa17d719df8ed4c6e1a
ahmed03 <142502232+AhmedRaza02912@users.noreply.github.com>
2026-07-05 00:49:14 +0500

    Initial Commit

---
+++ InvoiceProcessor/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+var app = builder.Build();
+
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+
+logger.LogInformation("Invoice Processor has started");
+
+await app.RunAsync();
